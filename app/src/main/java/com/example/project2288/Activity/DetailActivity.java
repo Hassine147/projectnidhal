8ef02ded86965d5ded3a7a9fdb9500f160deb210
hassinegreguiche13 <hassinegreguiche13@gmail.com>
2025-05-07 12:07:53 +0100

    Detail shop backen avancement

--- app/src/main/java/com/example/project2288/Activity/DetailActivity.java
+++ app/src/main/java/com/example/project2288/Activity/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.project2288.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +9,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.example.project2288.Domain.ItemDomain;
 import com.example.project2288.R;
+import com.example.project2288.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
-
+    ActivityDetailBinding binding;
+    private ItemDomain object;
+    private int weight=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding=ActivityDetailBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        
+        getBundles();
+        setVariable();
+
     }
+
+    private void setVariable() {
+        binding.backBtn.setOnClickListener(v -> finish());
+        Glide.with(DetailActivity.this)
+                .load(object.getImagePath())
+                .load(binding.img);
+        binding.pricekgTxt.setText(object.getPrice()+ " $/kg");
+    }
+
+    private void getBundles() {
+        object= (ItemDomain) getIntent().getSerializableExtra("object");
+    }
+
 }
